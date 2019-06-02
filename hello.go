cf205dda23162f3dbba171fe9941b4eb2ca28b9a
Anssi Eteläniemi <aetelani@hotmail.com>
2019-06-02 18:29:24 +0300

    variable parameters and callbacks

--- hello.go
+++ hello.go
@@ -12,10 +12,34 @@ type message interface {
 	getMessage() string
 }
 type local struct {
-	Me string;
+	Me string
 }
+
+// Implementing interface method in struct
 func (m local) getMessage() (string) {
 	return "foo"
+}
+
+// Variable paramaters
+func get42(params ... int) int {
+	fmt.Printf("Parameters given: %d\n", len(params))
+
+	// making list of interfaces amount of parameters
+	//is := make([]interface{}, len(params))
+	sum := 0;
+
+	// range give index to variable parameters
+	for i := range params {
+		fmt.Printf("%d,", params[i])
+		sum += params[i];
+	}
+	return sum;
+}
+
+// Callback
+func fun(foo func(...int) int, x, y int) int {
+	meaning := foo(x, y)
+	return meaning
 }
 
 func getMessage(i string) (string, bool) {
@@ -24,6 +48,9 @@ func getMessage(i string) (string, bool) {
 
 func main() {
 	l := local{Me: "Hellos"}
+	yep := fun(get42, 11, 22)
+	_ = yep
+	fmt.Printf("Meaning %d\n ", yep)
 	msg, _ := getMessage(l.Me)
 	fmt.Printf(msg)
 	nums := []int{2, 3, 4}
